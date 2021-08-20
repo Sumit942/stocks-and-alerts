a409bccc18a123fd5259690179cd2ff6be048f2c
Sumit Paswan <sumitpaswan942@gmail.com>
2021-08-21 04:02:21 +0530

    removing eager loading

--- src/main/java/com/stock/demo/entities/Stock.java
+++ src/main/java/com/stock/demo/entities/Stock.java
@@ -4,10 +4,8 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
